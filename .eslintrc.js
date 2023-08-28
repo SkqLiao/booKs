f0b786691a894983488aeeb6d503825047463b49
SkqLiao <skqliao@gmail.com>
2023-08-28 09:16:07 +0800

    build(projects): add eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,7 @@ module.exports = {
   rules: {
     'no-console': 'off',
     'vue/multi-word-component-names': 'off',
-    '@typescript-eslint/no-explicit-any': 'off'
+    '@typescript-eslint/no-explicit-any': 'off',
+    'vue/no-dupe-keys': 'off'
   }
 }
