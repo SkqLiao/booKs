a90531639ff10feb34b8b6c706d32f356933033f
SkqLiao <skqliao@gmail.com>
2023-09-06 15:34:55 +0800

    update

---
+++ excerpt/add.php
@@ -0,0 +1,10 @@
+<?php
+require("../func.php");
+
+$input_data = json_decode(file_get_contents("php://input"), true);  
+
+checkRequiredFields($input_data, ["book_id", "time_length", "start_page", "end_page", "date"]);
+
+addData("reading_record", $input_data);
+
+?>
