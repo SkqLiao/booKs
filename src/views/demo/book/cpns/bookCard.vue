a484eb2d59552e3d9165983ee8f3fadf2f20aee3
SkqLiao <skqliao@gmail.com>
2023-09-11 17:34:13 +0800

    refactor(components): modify author format \& height

--- src/views/demo/book/cpns/bookCard.vue
+++ src/views/demo/book/cpns/bookCard.vue
@@ -1,7 +1,18 @@
 <template>
   <div v-if="bookInfo">
     <n-card class="card" :hoverable="true">
-      <div class="card-content">
+      <template #header>
+        <n-button
+          text
+          @click="showDetailModal = true"
+          style="white-space: normal"
+        >
+          <h3>{{ bookInfo.title }}</h3>
+        </n-button>
+      </template>
+      <div
+        :style="props.cardType == 'default' ? 'height: 130px' : 'height: 120px'"
+      >
         <n-row>
           <n-col :span="6">
             <div class="cover-container">
@@ -10,71 +21,55 @@
           </n-col>
           <n-col :span="18">
             <div class="content">
-              <n-button
-                text
-                @click="showDetailModal = true"
-                style="white-space: normal"
-              >
-                <h3>{{ bookInfo.title }}</h3>
-              </n-button>
-              <div class="info">
+              <div :id="'info' + props.id">
                 <p>
                   作者：
-                  <span class="field-container">
-                    <n-button
-                      v-for="author in bookInfo.author"
-                      :key="author"
-                      text
-                      quaterary
-                      round
-                      @click="selectFilter('author', author)"
-                    >
-                      {{ author }}
-                    </n-button>
+                  <span
+                    v-for="(author, index) in bookInfo.author"
+                    :key="author"
+                    @click="selectFilter('author', author)"
+                  >
+                    {{
+                      author + (index < bookInfo.author.length - 1 ? ' / ' : '')
+                    }}
                   </span>
                 </p>
-                <p>
+                <p v-if="props.cardType == 'default'">
                   出版社：
-                  <span class="field-container">
-                    <n-button
-                      text
-                      quaterary
-                      round
-                      type="info"
-                      @click="selectFilter('publish', bookInfo.publish)"
-                    >
-                      {{ bookInfo.publish }}
-                    </n-button>
-                  </span>
+                  <n-button
+                    text
+                    quaterary
+                    round
+                    type="info"
+                    @click="selectFilter('publish', bookInfo.publish)"
+                  >
+                    {{ bookInfo.publish }}
+                  </n-button>
                 </p>
-                <p v-if="bookInfo.producer">
+                <p v-if="bookInfo.producer && props.cardType == 'default'">
                   出版方：
-                  <span class="field-container">
-                    <n-button
-                      text
-                      quaterary
-                      round
-                      type="primary"
-                      @click="selectFilter('producer', bookInfo.producer)"
-                    >
-                      {{ bookInfo.producer }}
-                    </n-button>
-                  </span>
+                  <n-button
+                    text
+                    quaterary
+                    round
+                    type="primary"
+                    @click="selectFilter('producer', bookInfo.producer)"
+                  >
+                    {{ bookInfo.producer }}
+                  </n-button>
                 </p>
-                <p v-if="bookInfo.series">
+                <p v-if="bookInfo.series && props.cardType == 'default'">
                   丛书：
-                  <span class="field-container">
-                    <n-button
-                      text
-                      quaterary
-                      round
-                      type="primary"
-                      @click="selectFilter('series', bookInfo.series)"
-                      style="white-space: normal"
-                    >
-                      {{ bookInfo.series }}
-                    </n-button>
-                  </span>
+                  <n-button
+                    text
+                    quaterary
+                    round
+                    type="primary"
+                    @click="selectFilter('series', bookInfo.series)"
+                    style="white-space: normal"
+                  >
+                    {{ bookInfo.series }}
+                  </n-button>
                 </p>
               </div>
             </div>
@@ -95,7 +90,9 @@
   </div>
   <div v-else>
     <n-card class="card" :hoverable="true">
-      <div class="card-content" />
+      <div
+        :style="props.cardType == 'default' ? 'height: 160px' : 'height: 120px'"
+      />
       <template #action>
         <span class="time">购买于</span>
       </template>
@@ -105,7 +102,7 @@
 
 <script setup lang="ts">
 import { getRequest, getInfo, bookCoverRequest } from '@/service/book/book'
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { Ibook } from '@/service/book/types'
 import bookDetail from './bookDetail.vue'
 import eventBus from '@/eventbus/index'
@@ -194,9 +191,6 @@ onMounted(async () => {
   flex-direction: column;
   justify-content: space-between;
 }
-.card-content {
-  height: 160px;
-}
 .cover-container {
   width: 100%;
   text-align: center;
@@ -212,7 +206,6 @@ onMounted(async () => {
   max-width: 100%;
   height: 150px;
   overflow: hidden;
-  display: flex;
   justify-content: center;
   align-items: center;
 }
@@ -225,7 +218,7 @@ onMounted(async () => {
   font-size: 12px;
   color: #999;
 }
-.field-container {
+.field-container-1 {
   max-height: 1.5em; /* Adjust this value as needed */
   overflow: hidden;
   white-space: nowrap;
