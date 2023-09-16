ab9120f27842f69c7153393cb35ad3fbae11683b
SkqLiao <skqliao@gmail.com>
2023-09-17 00:09:15 +0800

    upd

--- read/add.php
+++ read/add.php
@@ -3,29 +3,7 @@ require("../func.php");
 
 $input_data = json_decode(file_get_contents("php://input"), true);  
 
-checkRequiredFields($input_data, ["book_id", "time_length", "start_page", "end_page", "date", "finished"]);
+checkRequiredFields($input_data, ["book_id", "time_length", "start_page", "end_page", "start_time", "finished"]);
 
-$finished = $input_data["finished"] ? 1 : 0;
-unset($input_data["finished"]);
-
-addData("reading_record", $input_data, false);
-
-$result = queryRawData("SELECT * from reading_status WHERE book_id = {$input_data["book_id"]}");
-if ($result->num_rows > 0) {
-    updateData("reading_status", 
-    [
-        "finished" => $finished,
-        "date" => $input_data["date"]
-    ],
-    "book_id = {$input_data["book_id"]}");
-} else {
-    addData("reading_status", [
-        "book_id" => $input_data["book_id"],
-        "finished" => $finished,
-        "date" => $input_data["date"]
-    ]);
-}
-
-
-
+addData("reading_record", $input_data);
 ?>
